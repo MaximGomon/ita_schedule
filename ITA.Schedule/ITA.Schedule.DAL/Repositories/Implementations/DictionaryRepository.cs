0ed6342abd540c540d018ab0ec004f76da5287a5
Vladyslav Mykhaylyuk <vladyslaff87@gmail.com>
2016-11-11 17:44:30 +0200

    Added IDictionaryRepository interface and DictionaryRepository class

---
+++ ITA.Schedule/ITA.Schedule.DAL/Repositories/Implementations/DictionaryRepository.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq;
+using ITA.Schedule.DAL.Repositories.Interfaces;
+using ITA.Schedule.Entity;
+
+namespace ITA.Schedule.DAL.Repositories.Implementations
+{
+    /// <summary>
+    /// Repository to perform CRUD operations ove rictionary entities
+    /// </summary>
+    public class DictionaryRepository<DictionaryTEntity> : CrudRepository<DictionaryTEntity>, IDictionaryRepository<DictionaryTEntity>
+        where DictionaryTEntity : DictionaryEntity
+    {
+        // get dictionary entity from DB by code
+        public DictionaryTEntity GetByCode(int code)
+        {
+            return ContextDb.Set<DictionaryTEntity>().FirstOrDefault(x => x.Code == code);
+        }
+    }
+}
---
+++ ITA.Schedule/ITA.Schedule.DAL/Repositories/Interfaces/IDictionaryRepository.cs
@@ -0,0 +1,14 @@
+﻿using ITA.Schedule.Entity;
+
+namespace ITA.Schedule.DAL.Repositories.Interfaces
+{
+    /// <summary>
+    /// Interface to be implemented by Dictoionary repository
+    /// </summary>
+    /// <typeparam name="DictionaryEntity"></typeparam>
+    public interface IDictionaryRepository<DictionaryTEntity> where DictionaryTEntity : DictionaryEntity
+    {
+        // returns an entity found in the DB by code
+        DictionaryTEntity GetByCode(int code);
+    }
+}

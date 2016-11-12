6a7082f63d2e847acb6fbab3ca0b5c4d12c03c60
Vladyslav Mykhaylyuk <vladyslaff87@gmail.com>
2016-11-12 21:57:23 +0200

    Added group repository

---
+++ ITA.Schedule/ITA.Schedule.DAL/Repositories/Implementations/GroupRepository.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using ITA.Schedule.DAL.Repositories.Interfaces;
+using ITA.Schedule.Entity.Entities;
+
+namespace ITA.Schedule.DAL.Repositories.Implementations
+{
+    /// <summary>
+    /// To perform CRUD operations over Group entity
+    /// </summary>
+    public class GroupRepository : CrudRepository<Group>, IGroupRepository
+    {
+        // add a subgroup to a group
+        public void AddSubgroupToGroup(Guid groupId, Guid subgroupId)
+        {
+            var group = GetById(groupId);
+            group.SubGroups.Add(ContextDb.SubGroups.FirstOrDefault(x => x.Id == subgroupId));
+            Update(group);
+        }
+
+        // remove a subgroup from a group
+        public void UnlinkSubgroupFromGroup(Guid groupId, Guid subgroupId)
+        {
+            var group = GetById(groupId);
+            group.SubGroups.Remove(ContextDb.SubGroups.FirstOrDefault(x => x.Id == subgroupId));
+            Update(group);
+        }
+    }
+}
---
+++ ITA.Schedule/ITA.Schedule.DAL/Repositories/Interfaces/IGroupRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using ITA.Schedule.Entity.Entities;
+
+namespace ITA.Schedule.DAL.Repositories.Interfaces
+{
+    /// <summary>
+    /// To be implemented by GroupRepository
+    /// </summary>
+    public interface IGroupRepository : ICrudRepository<Group>
+    {
+        // add subgroup to a group
+        void AddSubgroupToGroup(Guid groupId, Guid subgroupId);
+        // unlink subgroup from a group
+        void UnlinkSubgroupFromGroup(Guid groupId, Guid subgroupId);
+    }
+}

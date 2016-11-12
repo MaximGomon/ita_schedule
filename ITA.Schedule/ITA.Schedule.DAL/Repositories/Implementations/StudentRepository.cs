76265cf32d69f33a54ae900e028604f5211c26b7
Vladyslav Mykhaylyuk <vladyslaff87@gmail.com>
2016-11-12 21:45:29 +0200

    Added Student Repo

---
+++ ITA.Schedule/ITA.Schedule.DAL/Repositories/Implementations/StudentRepository.cs
@@ -0,0 +1,13 @@
+﻿using ITA.Schedule.DAL.Repositories.Interfaces;
+using ITA.Schedule.Entity.Entities;
+
+namespace ITA.Schedule.DAL.Repositories.Implementations
+{
+    /// <summary>
+    /// To perform CRUD operations over Student entity
+    /// </summary>
+    public class StudentRepository : CrudRepository<Student>, IStudentRepository
+    {
+         
+    }
+}
---
+++ ITA.Schedule/ITA.Schedule.DAL/Repositories/Interfaces/IStudentRepository.cs
@@ -0,0 +1,12 @@
+﻿using ITA.Schedule.Entity.Entities;
+
+namespace ITA.Schedule.DAL.Repositories.Interfaces
+{
+    /// <summary>
+    /// Interface to be implemented by StudentRepo
+    /// </summary>
+    public interface IStudentRepository : ICrudRepository<Student>
+    {
+         //todo think about methods here
+    }
+}

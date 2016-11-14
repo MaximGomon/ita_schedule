7004b57be555d2cb03decc05d0cdc9eae1dd394b
viktorlv30 <viktor.lv.30@gmail.com>
2016-11-14 23:03:53 +0200

    Subject repository is created

--- ITA.Schedule/ITA.Schedule.BLL/Implementations/SubjectBl.cs
+++ ITA.Schedule/ITA.Schedule.BLL/Implementations/SubjectBl.cs
@@ -1,10 +1,14 @@
 ﻿using ITA.Schedule.BLL.Implementations.Base;
 using ITA.Schedule.BLL.Interface;
+using ITA.Schedule.DAL.Repositories.Interfaces;
 using ITA.Schedule.Entity.Entities;
 
 namespace ITA.Schedule.BLL.Implementations
 {
-    //public class SubjectBl : CrudBll<ISubjectRepository, Subject>, ISubjectBl
-    //{
-    //}
+    public class SubjectBl : CrudBll<ISubjectRepository, Subject>, ISubjectBl
+    {
+        public SubjectBl(ISubjectRepository repository) : base(repository)
+        {
+        }
+    }
 }

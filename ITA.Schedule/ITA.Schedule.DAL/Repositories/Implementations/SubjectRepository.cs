0460b00a78004e3b22849ec0da0631220f0d946f
Vladyslav Mykhaylyuk <vladyslaff87@gmail.com>
2016-11-14 20:21:46 +0200

    Added Subject repository

---
+++ ITA.Schedule/ITA.Schedule.DAL/Repositories/Implementations/SubjectRepository.cs
@@ -0,0 +1,13 @@
+﻿using ITA.Schedule.DAL.Repositories.Interfaces;
+using ITA.Schedule.Entity.Entities;
+
+namespace ITA.Schedule.DAL.Repositories.Implementations
+{
+    /// <summary>
+    /// Repository to perform CRUD operations ofver Subject entity
+    /// </summary>
+    public class SubjectRepository : CrudRepository<Subject>, ISubjectRepository
+    {
+         
+    }
+}

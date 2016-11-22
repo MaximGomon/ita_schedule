e77b10e0c0a38b406b7e68e9404dfd771591f57a
Vladyslav Mykhaylyuk <vladyslaff87@gmail.com>
2016-11-22 12:48:58 +0200

    Added Admin Controller and corresponding view

---
+++ ITA.Schedule/ITA.Schedule/Controllers/AdminController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ITA.Schedule.BLL.Implementations;
+using ITA.Schedule.DAL.Repositories.Implementations;
+
+namespace ITA.Schedule.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult Index()
+        {
+            var teacherBl = new TeacherBl(new TeacherRepository());
+            ViewBag.Teachers = teacherBl.GetAll();
+            return View("TeachersList");
+        }
+    }
+}

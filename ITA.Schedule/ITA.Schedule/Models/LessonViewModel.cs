84459312392045fd4c2962cf7a1ab6d28005dd85
Maxim Gomon <flymandude@gmail.com>
2017-02-01 09:50:15 +0200

    Little fix

--- ITA.Schedule/ITA.Schedule/Models/LessonViewModel.cs
+++ ITA.Schedule/ITA.Schedule/Models/LessonViewModel.cs
@@ -1,9 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Web.Mvc;
-using ITA.Schedule.Entity.Entities;
-using Microsoft.AspNetCore.Mvc.Rendering;
 using SelectListItem = System.Web.Mvc.SelectListItem;
 
 

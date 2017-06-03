45df7db2493184cbb2e7ab69cecae09112e5b9d0
Dima <dima-bubnovskyi@gmail.com>
2017-06-04 00:37:01 +0300

    Task #1997 - created design - created data sending to control - created ViewBag messages form & model

--- ITA.Schedule/ITA.Schedule/Controllers/HomeController.cs
+++ ITA.Schedule/ITA.Schedule/Controllers/HomeController.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using epamEntryTask.Controllers;
+using ITA.Schedule.BLL.Implementations;
+using ITA.Schedule.DAL.Repositories.Implementations;
+using ITA.Schedule.Entity.Entities;
 using ITA.Schedule.Models;
 
 
@@ -13,6 +17,11 @@ namespace ITA.Schedule.Controllers
         // GET: Authorization
         public ActionResult Authorization()
         {
+            ViewBag.AlertLogin = TempData["AlertLogin"];
+            ViewBag.AlertRegister = TempData["AlertRegister"];
+            TempData["AlertLogin"] = null;
+            TempData["AlertRegister"] = null;
+
             return View();
         }
 
@@ -22,5 +31,46 @@ namespace ITA.Schedule.Controllers
             return RedirectToAction("Index", "StudentScheduler");
         }
 
+        [HttpPost]
+        public ActionResult Login(string emailLogin, string passwordLogin)
+        {
+            var user = new UserBl(new UserRepository()).AuthorizeApp(emailLogin, passwordLogin);
+
+            if (user == null)
+            {
+                TempData["AlertLogin"] = new AlertsMassege
+                {
+                    Status = AlertsMassege.StatusesEnum.Danger,
+                    Tittle = "Error",
+                    Text = "In the database is no user with such Email address & password"
+                };
+                return RedirectToAction("Authorization");
+            }
+            TempData["AlertLogin"] = new AlertsMassege
+            {
+                Status = AlertsMassege.StatusesEnum.Success,
+                Tittle = "Success",
+                Text = "Success case of logsn"
+            };
+            return RedirectToAction("Authorization");
+        }
+
+        [HttpPost]
+        public ActionResult Register(
+            string firstNameRegister, 
+            string lastNameRegister,
+            string emailRegister, 
+            string passwordRegister,
+            string selectorRegister // value == Student or Teacher
+            )
+        {
+            TempData["AlertRegister"] = new AlertsMassege
+            {
+                Status = AlertsMassege.StatusesEnum.Warning,
+                Tittle = "Test",
+                Text = "Test"
+            };
+            return RedirectToAction("Authorization");
+        }
     }
 }
---
+++ ITA.Schedule/ITA.Schedule/Models/AlertsMessege.cs
@@ -0,0 +1,29 @@
+﻿namespace epamEntryTask.Controllers
+{
+    /// <summary>Special class for displaying messages on the home page using Bootstrap Alerts</summary>
+    public class AlertsMassege
+    {
+        /// <summary>Represents the types of notifications that Bootstrap Alerts contains</summary>
+        public enum StatusesEnum
+        {
+            Success,
+            Info,
+            Warning,
+            Danger
+        }
+        /// <summary>Current type of Bootstrap Alerts</summary>
+        public StatusesEnum Status { get; set; }
+        /// <summary>String that represents HTML class="name" from Bootstrap Alerts</summary>
+        public string[] ClassNames =
+        {
+            "alert-success",
+            "alert-info",
+            "alert-warning",
+            "alert-danger"
+        };
+        /// <summary>Massage Title</summary>
+        public string Tittle { get; set; }
+        /// <summary>Massage Text</summary>
+        public string Text { get; set; }
+    }
+}

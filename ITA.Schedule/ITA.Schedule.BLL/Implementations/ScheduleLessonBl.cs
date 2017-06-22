201ed0b75221bc8f88088c6e7bfae46924a63605
DESKTOP-7QP2SJT\Vitalii <kv111nt@gmail.com>
2017-06-22 14:23:12 +0300

    Task#1904 ADD INTO - ITA.Schedule.BLL\Implementations\SheduleLessonBl.cs  - Logger for current methods

--- ITA.Schedule/ITA.Schedule.BLL/Implementations/ScheduleLessonBl.cs
+++ ITA.Schedule/ITA.Schedule.BLL/Implementations/ScheduleLessonBl.cs
@@ -3,6 +3,7 @@ using ITA.Schedule.BLL.Implementations.Base;
 using ITA.Schedule.BLL.Interface;
 using ITA.Schedule.DAL.Repositories.Interfaces;
 using ITA.Schedule.Entity.Entities;
+using NLog;
 
 namespace ITA.Schedule.BLL.Implementations
 {
@@ -11,6 +12,7 @@ namespace ITA.Schedule.BLL.Implementations
     /// </summary>
     public class ScheduleLessonBl : CrudBll<IScheduleLessonRepository, ScheduleLesson>, IScheduleLessonBl
     {
+        private static readonly Logger _logger = LogManager.GetCurrentClassLogger();
         public ScheduleLessonBl(IScheduleLessonRepository repository) : base(repository)
         {
         }
@@ -18,6 +20,7 @@ namespace ITA.Schedule.BLL.Implementations
         // to check if a user has schediled lessons
         public bool IsWithScheduledLessons(Guid teacherId)
         {
+            _logger.Info("IsWithScheduledLessons ({0})", teacherId);
             return Repository.IsWithScheduledLessons(teacherId);
         }
     }

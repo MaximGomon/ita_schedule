169f087a880064da7d190a29e204440ee2b7f4dc
DESKTOP-7QP2SJT\Vitalii <kv111nt@gmail.com>
2017-06-22 14:39:41 +0300

    Task#1904 ADD INTO - ITA.Schedule.BLL\Implementations\SubGroupBl.cs  - Logger for current methods

--- ITA.Schedule/ITA.Schedule.BLL/Implementations/SubGroupBl.cs
+++ ITA.Schedule/ITA.Schedule.BLL/Implementations/SubGroupBl.cs
@@ -3,11 +3,13 @@ using ITA.Schedule.BLL.Implementations.Base;
 using ITA.Schedule.BLL.Interface;
 using ITA.Schedule.DAL.Repositories.Interfaces;
 using ITA.Schedule.Entity.Entities;
+using NLog;
 
 namespace ITA.Schedule.BLL.Implementations
 {
     public class SubGroupBl : CrudBll<ISubgroupRepository, SubGroup>, ISubGroupBl
     {
+        private static readonly Logger _logger = LogManager.GetCurrentClassLogger();
         public SubGroupBl(ISubgroupRepository repository) : base(repository)
         {
         }
@@ -15,11 +17,13 @@ namespace ITA.Schedule.BLL.Implementations
         public void AddStudentToSubgroup(Guid subgroupId, Guid studentId)
         {
             Repository.AddStudentToSubgroup(subgroupId, studentId);
+            _logger.Info("AddStudentToSubgroup ({0} , {1})", subgroupId, studentId);
         }
 
         public void RemoveStudentFromSubgroup(Guid subgroupId, Guid studentId)
         {
             Repository.RemoveStudentFromSubgroup(subgroupId, studentId);
+            _logger.Info("RemoveStudentFromSubgroup ({0} , {1})", subgroupId, studentId);
         }
     }
 }

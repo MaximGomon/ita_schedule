d2d5befc351e41ce829fa124304268acf2872588
DESKTOP-7QP2SJT\Vitalii <kv111nt@gmail.com>
2017-06-22 14:50:33 +0300

    Task#1904 ADD INTO - ITA.Schedule.BLL\Implementations\TeacherBl.cs  - Logger for current methods

--- ITA.Schedule/ITA.Schedule.BLL/Implementations/TeacherBl.cs
+++ ITA.Schedule/ITA.Schedule.BLL/Implementations/TeacherBl.cs
@@ -6,13 +6,14 @@ using ITA.Schedule.BLL.Interface;
 using ITA.Schedule.DAL.Repositories.Interfaces;
 using ITA.Schedule.Entity.Entities;
 using ITA.Schedule.Util;
+using NLog;
 
 
 namespace ITA.Schedule.BLL.Implementations
 {
     public class TeacherBl : CrudBll<ITeacherRepository, Teacher>, ITeacherBl
     {
-
+        private static readonly Logger _logger = LogManager.GetCurrentClassLogger();
         public TeacherBl(ITeacherRepository repository) : base(repository)
         {
 
@@ -21,30 +22,37 @@ namespace ITA.Schedule.BLL.Implementations
         public void AddSubjectToTeacher(Guid teacherId, Guid subjecId)
         {
             Repository.AddSubjectToTeacher(teacherId, subjecId);
+            _logger.Info("AddSubjectToTeacher ({0} , {1})", teacherId, subjecId);
         }
 
         public void DeleteSubjectFromTeacher(Guid teacherId, Guid subjecId)
         {
             Repository.DeleteSubjectFromTeacher(teacherId, subjecId);
+            _logger.Info("DeleteSubjectFromTeacher ({0} , {1})", teacherId, subjecId);
         }
 
         public bool SetTeacherBusy(Guid teacherId, LessonTime lessonTime, DateTime day)
         {
+            _logger.Info("SetTeacherBusy ({0} , {1} , {2})", teacherId, lessonTime, day);
             return Repository.SetTeacherBusy(teacherId, lessonTime, day);
+
         }
 
         public bool SetTeacherFree(Guid teacherId, LessonTime lessonTime, DateTime day)
         {
+            _logger.Info("SetTeacherFree ({0} , {1} , {2})", teacherId, lessonTime, day);
             return Repository.SetTeacherFree(teacherId, lessonTime, day);
         }
 
         public bool SetTeacherActive(Guid teacherId, LessonTime lessonTime, DateTime day)
         {
+            _logger.Info("SetTeacherActive ({0} , {1} , {2})", teacherId, lessonTime, day);
             return Repository.SetTeacherActive(teacherId, lessonTime, day);
         }
 
         public bool SetTeacherInactive(Guid teacherId, LessonTime lessonTime, DateTime day)
         {
+            _logger.Info("SetTeacherInactive ({0} , {1} , {2})", teacherId, lessonTime, day);
             return Repository.SetTeacherInactive(teacherId, lessonTime, day);
         }
 
@@ -61,13 +69,14 @@ namespace ITA.Schedule.BLL.Implementations
                     teachersFree.Add(teacher);
                 }
             }
-
+            _logger.Info("GetFreeTeacherOnDate ({0})", date);
             return teachersFree;
         }
 
         // update teacher name and subjects
         public bool UpdateTeacher(Guid teacherId, string newName, List<Guid> subjectsIds)
         {
+            _logger.Info("UpdateTeacher ({0} , {1} , {2})", teacherId, newName, subjectsIds);
             // if something is wrong with new name
             if (newName == String.Empty || newName.Length > 400)
             {

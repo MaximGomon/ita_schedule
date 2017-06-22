ba8727d32143b498fcaf2a52e7f77e12a48b4cfe
DESKTOP-7QP2SJT\Vitalii <kv111nt@gmail.com>
2017-06-22 14:56:27 +0300

    Task#1904 ADD INTO - ITA.Schedule.BLL\Implementations\UserBl.cs  - Logger for current methods

--- ITA.Schedule/ITA.Schedule.BLL/Implementations/UserBl.cs
+++ ITA.Schedule/ITA.Schedule.BLL/Implementations/UserBl.cs
@@ -5,23 +5,27 @@ using ITA.Schedule.BLL.Interface;
 using ITA.Schedule.DAL.Repositories.Interfaces;
 using ITA.Schedule.Entity.Entities;
 using ITA.Schedule.Util;
+using NLog;
 
 namespace ITA.Schedule.BLL.Implementations
 {
     public class UserBl : CrudBll<IUserRepository, User>, IUserBl
     {
+        private static readonly Logger _logger = LogManager.GetCurrentClassLogger();
         public UserBl(IUserRepository repository) : base(repository)
         {
         }
 
         public User AuthorizeApp(string login, string password)
         {
+            _logger.Info("AuthorizeApp ({0} , {1})", login, password);
             return Repository.AuthorizeApp(login, password);
         }
 
         // create new user and insert to the Db
         public bool CreateNewUser(string login, string password, Guid ownerId, UserType type)
         {
+            _logger.Info("CreateNewUser ({0} , {1} , {2} , {3})", login, password, ownerId, type);
             var user = new User();
             // check if unique login
             if (login == null || Repository.Get(x => x.Login == login).FirstOrDefault() != null)
@@ -80,18 +84,21 @@ namespace ITA.Schedule.BLL.Implementations
         // attach student to the user
         public Student AttachStudent(Guid studentId)
         {
+            _logger.Info("AttachStudent ({0})", studentId);
             return Repository.AttachStudent(studentId);
         }
 
         // attache teacher to the user
         public Teacher AttachTeacher(Guid teachreId)
         {
+            _logger.Info("AttachTeacher ({0})", teachreId);
             return Repository.AttachTeacher(teachreId);
         }
 
         // set user security group
         public SecurityGroup SetSecurityGroup(string groupName)
         {
+            _logger.Info("SetSecurityGroup ({0})", groupName);
             return Repository.SetSecurityGroup(groupName);
         }
     }

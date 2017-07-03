76da8a9b739b4abae44347c1b33c29669c4434e9
Dima <dima-bubnovskyi@gmail.com>
2017-07-03 14:04:54 +0300

    Small edits

--- ITA.Schedule/ITA.Schedule/Controllers/HomeController.cs
+++ ITA.Schedule/ITA.Schedule/Controllers/HomeController.cs
@@ -1,20 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using ITA.Schedule.Models;
 using ITA.Schedule.BLL.Implementations;
 using ITA.Schedule.DAL.Repositories.Implementations;
 using ITA.Schedule.Entity.Entities;
-using ITA.Schedule.Util;
 
-using System.Net.Mail;
 using ITA.Schedule.Logs.Filters;
-using NLog;
-using NLog.Fluent;
-
-//using for email sending
 
 
 namespace ITA.Schedule.Controllers
@@ -26,8 +16,6 @@ namespace ITA.Schedule.Controllers
         [ActionLog]
         public ActionResult Authorization()
         {
-            ViewBag.AlertsMessege = TempData["a"];
-
             return View();
         }
 
@@ -35,9 +23,10 @@ namespace ITA.Schedule.Controllers
         [HttpPost]
         public ActionResult Login(UserViewModel userModel)
         {
-            User user = null;
-            return  !ModelState.IsValidField("Email") && !ModelState.IsValidField("Password") ? SetAlertsMessege(userModel, new AlertsMessege().LoginFormNotValid()) : 
-                    !TryToAuthorizeUser(out user, userModel) ? SetAlertsMessege(userModel, new AlertsMessege().LoginNoMatchesInDb()) :
+            User user;
+            return  !ModelState.IsValidField("Email") && !ModelState.IsValidField("Password") ? 
+                        SetAlertsMessege(userModel, new AlertsMessege().LoginFormNotValid())  : 
+                    TryToAuthorizeUser(out user, userModel) ? SetAlertsMessege(userModel, new AlertsMessege().LoginNoMatchesInDb()) :
                     user.SecurityGroup.Name == "Admin"   ? RedirectToAction( "Index", "Admin",   new { area = "Admin" })   :
                     user.SecurityGroup.Name == "Student" ? RedirectToAction( "Index", "Student", new { area = "Student" }) :
                     user.SecurityGroup.Name == "Teacher" ? RedirectToAction( "Index", "Teacher", new { area = "Teacher" }) :
@@ -48,78 +37,38 @@ namespace ITA.Schedule.Controllers
         [HttpPost]
         public ActionResult Register(UserViewModel userModel)
         {
-            User user = null;
-            bool fuuu = GetUserByLogin(out user, userModel);
-            return !ModelState.IsValidField("Email") && !ModelState.IsValidField("Password") &&
+            User user;
+            return  !ModelState.IsValidField("Email") && !ModelState.IsValidField("Password") &&
                     !ModelState.IsValidField("FirstName") && !ModelState.IsValidField("LastName") && !ModelState.IsValidField("Role") ?
                         SetAlertsMessege(userModel, new AlertsMessege().RegisterFormNotValid()) :
-                    !GetUserByLogin(out user, userModel) ? SetAlertsMessege(userModel, new AlertsMessege().RegisterEmailAlreadyExist()) :
+                    GetUserByLogin(out user, userModel) ? SetAlertsMessege(userModel, new AlertsMessege().RegisterEmailAlreadyExist()) :
 
-                    SetAlertsMessege(userModel, new AlertsMessege()
+                    SetAlertsMessege(userModel, new AlertsMessege
                     {
                         Status = AlertsMessege.StatusesEnum.Info,
                         Tittle = "I don't know what to do further!",
-                        Text   = "do something someone!"
+                        Text   = "Do someone something!"
                     })
 
                     ;
-
-            //if (new UserBl(new UserRepository()).CreateNewUser(
-            //    emailRegister,
-            //    passwordRegister,
-            //    Guid.NewGuid(),
-            //    selectorRegister == "Student" ? UserType.Student : UserType.Teacher)
-            //    )
-            //{
-            //    #region -- Email sending --
-            //    MailMessage mail = new MailMessage("testEmailSend@yourcompany.com", emailRegister);
-            //    SmtpClient client = new SmtpClient
-            //    {
-            //        Port = 25,
-            //        DeliveryMethod = SmtpDeliveryMethod.Network,
-            //        UseDefaultCredentials = false,
-            //        Host = "smtp.google.com"
-            //    };
-            //    mail.Subject = "INTITA Schedule registration";
-            //    mail.Body = "Congratulations! You just registered on INTITA Schedule \r\nlogin email: " 
-            //        + emailRegister + "\n\npassword: " + passwordRegister;
-            //    client.Send(mail);
-            //    #endregion
-
-            //    TempData["AlertLogin"] = new AlertsMessege
-            //    {
-            //        Status = AlertsMessege.StatusesEnum.Success,
-            //        Tittle = "Success",
-            //        Text = "You have successfully registered, please try logging in using your email and password. " +
-            //               "Also we sent you mail with your registration data"
-            //    };
-            //    return RedirectToAction("Authorization");
-            //}
-
-            //TempData["AlertRegister"] = new AlertsMessege
-            //{
-            //    Status = AlertsMessege.StatusesEnum.Danger,
-            //    Tittle = "Error",
-            //    Text = "Something went wrong!"
-            //};
         }
 
         public bool TryToAuthorizeUser(out User user, UserViewModel userModel)
         {
             user = new UserBl(new UserRepository()).AuthorizeApp(userModel.Email, userModel.Password);
-            return user != null ? true : false;
+            return user == null;
         }
 
         public bool GetUserByLogin(out User user, UserViewModel userModel)
         {
             user = new UserBl(new UserRepository()).GetByLogin(userModel.Email);
-            return user != null ? true : false;
+            return user != null;
         }
 
-        public ActionResult SetAlertsMessege(UserViewModel userModel, AlertsMessege Messege)
+        public ActionResult SetAlertsMessege(UserViewModel userModel, AlertsMessege messege)
         {
-            TempData["a"] = Messege;
-            return RedirectToAction("Authorization", userModel);
+            ViewBag.AlertsMessege = messege;
+            return View("Authorization", userModel);
         }
     }
 }

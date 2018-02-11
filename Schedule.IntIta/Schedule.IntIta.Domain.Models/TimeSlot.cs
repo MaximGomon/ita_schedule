41d0762ae9afa731d17652bd5bf687d8c259245f
Mikhail Efremov <crv.show@gmail.com>
2018-02-11 19:45:50 +0200

    add time slots

---
+++ Schedule.IntIta/Schedule.IntIta.Domain.Models/TimeSlot.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Schedule.IntIta.Domain.Models
+{
+    /// <summary>Represents the time slot betwen start and end event.</summary>
+    public class TimeSlot
+    {
+        /// <summary>Unique Id of time slot.</summary>
+        public int Id { get; set; }
+
+        /// <summary>Start time in full format with date and time. Must be datetime2 in MsSQL, or DATETIME in MsSql</summary>
+        public DateTime StartTime { get; set; }
+        
+        /// <summary>End time in full format with date and time. Must be datetime2 in MsSQL, or DATETIME in MsSql</summary>
+        public DateTime EndTime { get; set; }
+        
+        /// <summary>Represent the Id of time slots type.</summary>
+        public int IdType { get; set; }
+    }
+}
---
+++ Schedule.IntIta/Schedule.IntIta.Domain.Models/TimeSlotTypes.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Schedule.IntIta.Domain.Models
+{
+    /// <summary>Represents the type of time slot.</summary>
+    public class TimeSlotTypes
+    {
+        /// <summary>Unique identificator of type. This field must have [Key] attribute.</summary>
+        [Key]
+        public int Id { get; set; }
+
+        /// <summary>Represents the name of type. This field must be [Required].</summary>
+        [Required, MinLength(2), MaxLength(20)]
+        public string Type { get; set; }
+    }
+}

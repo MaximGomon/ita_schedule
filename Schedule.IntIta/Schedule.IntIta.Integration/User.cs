6fc8971fb1d93c271caf0e4bffbfb7d6186cea4f
NazikXY <32540399+NazikXY@users.noreply.github.com>
2018-02-12 20:36:07 +0200

    Add User CRUD function

---
+++ Schedule.IntIta/Schedule.IntIta.Integration/User.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Schedule.IntIta.Integration
+{
+    class User
+    {
+        public string Login { get; set; }
+        public string Email { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public Grant[] Grants;
+
+        public User() {}
+
+        public User(string log, string e, string fn, string ln)
+        {
+            Login = log;
+            Email = e;
+            FirstName = fn;
+            LastName = ln;
+        }
+
+        public User CreateUser()
+        {
+            return new User();
+        }
+
+        public User ReadUser(int id)
+        {
+            return this;
+        }
+
+        public void UpdateUser(User user, int Id)
+        {
+            
+        }
+
+        public bool DeleteUser(int id)
+        {
+            return false;
+        }
+
+    }
+    
+}

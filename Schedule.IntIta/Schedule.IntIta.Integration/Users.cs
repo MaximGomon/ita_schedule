3b4a2ed892ed0c1ae9767e08cbb140c6b7a2ac6c
NazikXY <32540399+NazikXY@users.noreply.github.com>
2018-02-12 20:00:02 +0200

    Some doing

--- Schedule.IntIta/Schedule.IntIta.Integration/Users.cs
+++ Schedule.IntIta/Schedule.IntIta.Integration/Users.cs
@@ -10,7 +10,7 @@ namespace Schedule.IntIta.Integration
         public string Email { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        public Grant[] grants;
+        public Grant[] Grants;
     }
     
 }

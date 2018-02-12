c655ce21fe3893dd8c6c59c3e968b45e802d2a38
maxy3d <maxy3x@gmail.com>
2018-02-12 17:57:10 +0200

    added classes teacher & subject

---
+++ Schedule.IntIta/Schedule.IntIta.Domain.Models/Subject.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Schedule.IntIta.Domain.Models
+{
+    class Subject
+    {
+        public Guid Id { get; set; }
+        public string Name
+        {
+            get => Name;
+            set
+            {
+                if (value.Length > 0) Name = value;
+            }
+        }
+    }
+}
---
+++ Schedule.IntIta/Schedule.IntIta.Domain.Models/Teacher.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Schedule.IntIta.Domain.Models
+{
+    class Teacher
+    {
+        public string FirstName
+        {
+            get => FirstName;
+            set
+            {
+                if (value.Length > 0) FirstName = value;
+            }
+        }
+        public string LastName
+        {
+            get => LastName;
+            set
+            {
+                if (value.Length > 0) LastName = value;
+            }
+        }
+        public string Email
+        {
+            get => Email;
+            set
+            {
+                if (value.Length > 0) Email = value;
+            }
+        }
+        public Subject[] Subjects;
+        public TimeSlot[] TimeSlots;
+    }
+}

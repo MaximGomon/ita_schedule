6e783a8995a1d46b6557f8215f3aeade4dec9b25
RedSunflower <tanyablin@gmail.com>
2018-02-12 19:41:33 +0200

    created Room class

---
+++ Schedule.IntIta/Schedule.IntIta.Domain.Models/Room.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Schedule.IntIta.Domain.Models
+{
+    public class Room : DeletableEntity,ICrud
+    {
+        public int SeatNumber { get; set; }
+        public string Name { get; set; }
+        public enum Status
+        {
+            IsActive,
+            IsReserved,
+            IsUnavaliable
+        };
+        public void Add()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Read()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update()
+        {
+            throw new NotImplementedException();
+        }
+
+               
+
+        
+    }
+    
+
+}

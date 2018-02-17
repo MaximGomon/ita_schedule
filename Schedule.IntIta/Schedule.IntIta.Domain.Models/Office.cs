eaa3b11473a788d5ef62df8194818aa56dfe755a
IgorGomel <Gomel051184@gmail.com>
2018-02-17 16:18:42 +0200

    add lesson and office entity

---
+++ Schedule.IntIta/Schedule.IntIta.Domain.Models/Office.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Schedule.IntIta.Domain.Models
+{
+    public class Office : IdEntity
+    {
+        public string Name { get; set; }
+        public string Adress { get; set; }
+    }
+}

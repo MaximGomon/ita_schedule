e4c9ebd5169c9e6398c7fedc33a087481fcbe360
DELL6440\Max <maxy3x@gmail.com>
2018-02-26 20:02:00 +0200

    try to create some nonexisting tables

---
+++ Schedule.IntIta/Schedule.IntIta.DataAccess/Migrations/20180226175943_CreateOtherTables.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace Schedule.IntIta.DataAccess.Migrations
+{
+    public partial class CreateOtherTables : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}

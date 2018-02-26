4801d2ab3f53136a27da204cd2ce9f25807740bd
n.kubliy <n.kubliy@gmail.com>
2018-02-26 20:12:49 +0200

    SearchController & View

---
+++ Schedule.IntIta/Schedule.IntIta/Controllers/SearchController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Schedule.IntIta.Controllers
+{
+    public class SearchController : Controller
+    {
+        public IActionResult Index()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(int id)
+        {
+            return View();
+        }
+    }
+}

ba17422f5f9fbe70942f47185e44e0c60c017a65
n.kubliy <n.kubliy@gmail.com>
2018-02-28 17:38:16 +0200

    try to implement UserIntegration

--- Schedule.IntIta/Schedule.IntIta.Integration/UserIntegration.cs
+++ Schedule.IntIta/Schedule.IntIta.Integration/UserIntegration.cs
@@ -1,14 +1,27 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Net;
 using Microsoft.AspNetCore.Http;
 using Schedule.Intita.ApiRequest;
 using Schedule.IntIta.Domain.Models;
 using Newtonsoft.Json;
+using Schedule.Intita.ApiRequest.Enumerations;
 
 namespace Schedule.IntIta.Integration
 {
     public class UserIntegration
-    {
-        
+    { 
+        public static List<User> GetUserList()
+        {
+            ApiRequest<List<User>> apiRequest = new ApiRequest<List<User>>();
+            var response = apiRequest.Url("http://localhost:53649/Search/result")
+                .Authenticate()
+                .Get()
+                .Send();
+            
+
+            return response.Response;
+        }
     }
 }
--- Schedule.IntIta/Schedule.IntIta/Controllers/SearchController.cs
+++ Schedule.IntIta/Schedule.IntIta/Controllers/SearchController.cs
@@ -3,22 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Schedule.IntIta.BusinessLogic;
+using Schedule.IntIta.DataAccess;
+using Schedule.IntIta.Domain.Models;
+using Schedule.IntIta.Integration;
 
 namespace Schedule.IntIta.Controllers
 {
     public class SearchController : Controller
     {
+        private readonly IUserRepository _repository = new UserRepository();
+
         public IActionResult Index()
         {
-
             return View();
         }
-
         
         public IActionResult SearchResult(int id)
         {
+            UserBusinessLogic userBusinessLogic = new UserBusinessLogic(_repository);
+            List<User> users = new List<User> {userBusinessLogic.Read(id)};
             ViewBag.UserId = id;
-            return View();
+            return View(users);
         }
     }
 }

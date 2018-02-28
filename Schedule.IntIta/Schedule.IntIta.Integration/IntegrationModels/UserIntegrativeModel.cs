ace30a87f979750da7e5779bfc02ff0b3777c88d
n.kubliy <n.kubliy@gmail.com>
2018-02-28 18:47:44 +0200

    add Integrative User model

---
+++ Schedule.IntIta/Schedule.IntIta.Integration/IntegrationModels/UserIntegrativeModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Schedule.IntIta.Integration.IntegrationModels
+{
+    class UserIntegrativeModel
+    {
+        public int id { get; set; }
+        public string firstName { get; set; }
+        public string middleName { get; set; }
+        public string secondName  { get; set; }
+        public string email { get; set; }
+    }
+}

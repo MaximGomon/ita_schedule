d4050df927587b6f5e6e8b6933f51c474a1744cc
Mikhail Efremov <crv.show@gmail.com>
2018-03-13 22:14:12 +0200

    Update events db. Add encryption library.

---
+++ Schedule.IntIta/BusinessLogic.Test/CryptoTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using FakeItEasy;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Schedule.IntIta.BusinessLogic;
+using Schedule.IntIta.DataAccess;
+using Schedule.IntIta.DataAccess.Addons;
+using Schedule.IntIta.Domain.Models;
+
+namespace BusinessLogic.Test
+{
+    [TestClass]
+    public class CryptoTest
+    {
+        [TestMethod]
+        public void CryptDecryptTest()
+        {
+            string password = "PDFJSCS";
+
+            Crypto crypto = new Crypto(password, Crypto.RandomString(32), Crypto.RandomString(16));
+
+            string somepass = "boberESTmashu";
+
+            var superSecretPassword = crypto.Encrypt(somepass);
+            var returnPass = crypto.Decrypt(superSecretPassword);
+
+
+            Assert.IsNotNull(returnPass);
+            Assert.AreEqual(somepass, returnPass);
+        }
+    }
+}
--- Schedule.IntIta/Schedule.IntIta.Domain.Models/Event.cs
+++ Schedule.IntIta/Schedule.IntIta.Domain.Models/Event.cs
@@ -3,10 +3,10 @@
     public class Event : DeletableEntity
     {
         public EventType TypeOfEvent { get; set; }
-        public int SubjectId { get; set; }
-        public int InitiatorId { get; set; }
-        public int RoomId { get; set; }
-        public int GroupId { get; set; }
+        public int? SubjectId { get; set; }
+        public int? InitiatorId { get; set; }
+        public int? RoomId { get; set; }
+        public int? GroupId { get; set; }
         public TimeSlot Date { get; set; }
         public string Comments { get; set; }
     }

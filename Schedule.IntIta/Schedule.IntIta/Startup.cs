ac0a88d49a6b5558c140e96e3c71db6c75733d14
RedSunflower <tanyablin@gmail.com>
2018-03-31 13:46:08 +0300

    Cache Group

--- Schedule.IntIta/Schedule.IntIta/Startup.cs
+++ Schedule.IntIta/Schedule.IntIta/Startup.cs
@@ -16,7 +16,8 @@ using Schedule.IntIta.BusinessLogic;
 using Schedule.IntIta.Controllers;
 using Schedule.IntIta.DataAccess;
 using Schedule.IntIta.Integration;
-
+using Schedule.IntIta.Cache.Cache;
+using Schedule.IntIta.Domain.Models;
 
 namespace Schedule.IntIta
 {
@@ -33,6 +34,11 @@ namespace Schedule.IntIta
             services.AddAutoMapper();
             
             services.AddSingleton<ISubjectRepository, SubjectRepository>();
+
+            services.AddSingleton<ICacheStore<Group>, CacheStore<Group>>();
+            services.AddSingleton<IDataProvider<Group>, GroupDataProvider>();
+            services.AddSingleton<ICacheManager<Group>, CacheManager<Group>>();
+
             services.AddSingleton<ISubjectBusinessLogic, SubjectBusinessLogic>();
             services.AddSingleton<IEventBusinessLogic, EventBusinessLogic>();
             services.AddSingleton<IEventRepository, EventRepository>();

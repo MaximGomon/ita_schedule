05674433c2294c7e13b447019e7e801e3a7f2c75
maxy3d <Lfyz131011>
2018-05-04 23:28:34 +0300

    added filters on schedule

--- Schedule.IntIta/Schedule.IntIta/Controllers/ScheduleController.cs
+++ Schedule.IntIta/Schedule.IntIta/Controllers/ScheduleController.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Schedule.IntIta.BusinessLogic;
+using Schedule.IntIta.Domain.Models;
 using Schedule.IntIta.ViewModels;
 
 namespace Schedule.IntIta.Controllers
@@ -13,9 +15,11 @@ namespace Schedule.IntIta.Controllers
     public class ScheduleController : Controller
     {
         private readonly IEventBusinessLogic _eventBusinessLogic;
-        public ScheduleController(IEventBusinessLogic eventBusinessLogic)
+        private readonly IMapper _mapper;
+        public ScheduleController(IEventBusinessLogic eventBusinessLogic, IMapper mapper)
         {
             _eventBusinessLogic = eventBusinessLogic;
+            _mapper = mapper;
         }
         public IActionResult Index(int?RoomId = null)
         {
@@ -31,6 +35,58 @@ namespace Schedule.IntIta.Controllers
             }
            
             return View();
+        }
+        [HttpPost]
+        public ActionResult GetEventsForSchedule(FilterEvents filtersEvents = null)
+        {
+            List<EventViewModel> models = new List<EventViewModel>();
+
+            var initiatorFilter = filtersEvents.InitiatorName;
+            var eventTypeFilter = filtersEvents.TypeOfEvent;
+            var roomFilter = filtersEvents.RoomName;
+            var groupFilter = filtersEvents.GroupName;
+
+            List<CalendarEventViewModel> list = new List<CalendarEventViewModel>();
+            List<Group> groups = (List<Group>)_eventBusinessLogic.GetAllGroups();
+
+            var events = _eventBusinessLogic
+                    .GetAll()
+                    .Where(@event =>
+                        initiatorFilter != null ?
+                        (@event.InitiatorId != null
+                        &&
+                        _eventBusinessLogic.FindUsers(initiatorFilter.ToUpper())//search users at INTITA
+                            .Select(w => w.Id)//select only Ids of find users
+                            .Contains(@event.InitiatorId.Value)) : true
+                        &&
+                        eventTypeFilter != null ?
+                        (@event.TypeOfEvent != null
+                        &&
+                        @event.TypeOfEvent.Name.ToUpper().Contains(eventTypeFilter.ToUpper())) : true
+                        &&
+                        roomFilter != null ?
+                        (@event.RoomId != null
+                        &&
+                         _eventBusinessLogic.GetRoomById(@event.RoomId.Value).Name.ToUpper().Contains(roomFilter.ToUpper())) : true
+                        &&
+                        groupFilter != null ?
+                        (@event.GroupId != null
+                        &&
+                         _eventBusinessLogic.GetGroupById(@event.GroupId.Value).Name.ToUpper().Contains(groupFilter.ToUpper())) : true).ToList();
+
+
+
+            foreach (var item in events)
+            {
+                var calendarEvent = _mapper.Map<CalendarEventViewModel>(item);
+                calendarEvent.Group = groups.FirstOrDefault(x => x.Id == item.GroupId);
+                calendarEvent.Initiator = _eventBusinessLogic.GetUsersById(item.InitiatorId);
+                calendarEvent.Room = _eventBusinessLogic.GetAllRooms().FirstOrDefault(w => w.Id == item.RoomId);
+                calendarEvent.Subject = _eventBusinessLogic.GetSubjects().Single(x => x.Id == item.SubjectId);
+                list.Add(calendarEvent);
+            }
+
+            return new JsonResult(list);
         }
     }
 }
